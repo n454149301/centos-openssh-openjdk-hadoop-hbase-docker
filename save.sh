a5559e8d6f253ce93f91b911347e61f7b07dca8a
duan <naonao800@163.com>
2020-11-25 17:40:01 +0800

    包下错了

--- save.sh
+++ save.sh
@@ -3,4 +3,4 @@
 rm -rfd n454149301-centos-openssh-openjdk-hadoop-hbase-2.2.2.tar
 rm -rfd n454149301-centos-openssh-openjdk-hadoop-hbase-2.2.2.tar.xz
 sudo docker save "n454149301/centos-openssh-openjdk-hadoop-hbase:2.2.2" -o n454149301-centos-openssh-openjdk-hadoop-hbase-2.2.2.tar
-sudo xz n454149301-centos-openssh-openjdk-hadoop-hbase-2.2.2.tar
+sudo xz -v n454149301-centos-openssh-openjdk-hadoop-hbase-2.2.2.tar
